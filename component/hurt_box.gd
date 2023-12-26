36cc703563d847b9b22dd703e5f60b61b7b02e33
ash1ta <haiyu19931121@163.com>
2023-12-26 16:37:02 +0800

    hit effect

--- component/hurt_box.gd
+++ component/hurt_box.gd
@@ -8,6 +8,7 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	
 func _on_area_entered(area: Area2D) -> void:
+	print(area.owner.name)
 	if not area is HitBox:
 		return
 	var hitbox := area as HitBox
