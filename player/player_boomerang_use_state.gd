e166d23eb9b3853216b489c75742f8c6bf16e2c3
ash1ta <haiyu19931121@163.com>
2023-12-27 22:20:25 +0800

    Update player_boomerang_use_state.gd

--- player/player_boomerang_use_state.gd
+++ player/player_boomerang_use_state.gd
@@ -3,15 +3,19 @@ extends PlayerState
 @onready var boomerang_shooter: BoomerangAbilityController = %BoomerangAbilityController
 
 func on_enter() -> void:
-	shoot()
-	await player.animation_tree.animation_finished
-	if state_machine.current_state.name == "PlayerDeadState":
-		return
+	if boomerang_shooter.can_generate:
+		shoot()
+	else:
+		await get_tree().process_frame
+		if state_machine.current_state.name == "PlayerDeadState":
+			return
+		state_machine.change_state("PlayerIdleState")
+
 	
-	state_machine.change_state("PlayerIdleState")
 
 
 func shoot() -> void:
+
 	player.velocity = Vector2.ZERO
 	var direction: Vector2 = player.face_direction
 	if player.input_direction.length() > 0:
@@ -20,3 +24,9 @@ func shoot() -> void:
 	player.animation_tree["parameters/Boomerang/blend_position"] = player.face_direction
 	player.anim_playback.travel("Boomerang")
 	player.shield.boomerang(player.face_direction)
+	
+	await player.animation_tree.animation_finished
+	if state_machine.current_state.name == "PlayerDeadState":
+		return
+	
+	state_machine.change_state("PlayerIdleState")
